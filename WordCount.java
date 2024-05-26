405f08b7457809cc94155407695002896b243a1e
Abhinav Ashish <55495151+Abhinav7903@users.noreply.github.com>
2024-05-26 12:06:07 +0530

    first commit

---
+++ WordCount.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class WordCount {
+    public static int wordCount(String str) {
+        if (str == null || str.isEmpty())
+            return 0;
+        String[] words = str.split("\\s+");
+        return words.length;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Hello world!";
+        int count = wordCount(sentence);
+        System.out.println("Word count: " + count);
+    }
+}
